e29408dccb0926969a5f16f5211145ae7c8482f1
grovince <grovinceln@gmail.com>
2024-05-18 19:19:30 +0900

    add AppConfig file

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,13 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // 추상화 역할만 한다
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    private final MemberRepository memberRepository;
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,9 +8,16 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository(); //레파지토리에서 회원 검색해야됨
-    private DiscountPolicy discountPolicy;// 정액 할인 정책 --> 이거 지금 연결점이 없음 여기부터 다시 시작해야됨
-    // 현재 상황 : createOrder에서 쓰려고 하는 discountPolicy 객체와 연결된 discount클래스가 없다 -> AppConfig로 연결해야됨
+    private final MemberRepository memberRepository; //레파지토리에서 회원 검색해야됨
+    private final DiscountPolicy discountPolicy;// 정액 할인 정책
+    // => 이렇게 코드를 만들면 어떤 클래스를 사용하여 MemberRepository, DiscountPolicy 객체가 만들어지는지 알 수 없기 때문에
+    //    OrderServiceImpl 클래스는 추상화 역할만 가지고 있다는 것을 알 수 있다
+
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
