604ecbec47cd6e634dbced081daa89480b2b79d2
grovince <grovinceln@gmail.com>
2024-06-08 23:52:12 +0900

    singleton code

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -16,6 +16,10 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     // 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+    //MemoryMemberRepository()가 2번 호출된다
+
     @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
